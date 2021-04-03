6b42c04bbc7d3568ed52ec323988844d604924cd
zqm64 <zqm64@outlook.com>
2021-04-03 22:54:57 +0800

    msm8937-common: biometrics: Enable Fingerprint hacks

--- biometrics/product.mk
+++ biometrics/product.mk
@@ -1,3 +1,6 @@
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/xiaomi/msm8937-common/biometrics/overlay-lineage
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
