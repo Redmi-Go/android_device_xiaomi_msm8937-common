a8d7dee1210c298a68b5c8a90f092c69dd84253b
SakilMondal <mondalsakil@gmail.com>
2021-08-26 08:23:07 +0000

    msm8937-common: Switch to FBE
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -204,10 +204,6 @@ PRODUCT_PACKAGES += \
     ims_ext_common.xml \
     vendor.qti.hardware.camera.device@1.0
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
